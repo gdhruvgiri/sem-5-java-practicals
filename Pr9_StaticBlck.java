605a5f46eb40eaf44b22ed41cb2b77278ca63bad
Dhruvgiri <goswamidhruv411@gmail.com>
2022-12-03 17:51:39 +0530

    Added Practical 9

---
+++ Pr9_StaticBlck.java
@@ -0,0 +1,8 @@
+class Pr9_StaticBlck {
+    public static void main(String[] a){
+        System.out.println("Main Function !");
+    }
+    static {
+        System.out.println("Static Block !");
+    }
+}
